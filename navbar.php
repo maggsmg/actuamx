38271d68036154718a3108d97a7ae1fcd73078e1
Hector Rincon <hecerinc@hotmail.com>
2018-03-12 02:45:01 -0600

    Fix link to home

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <header class="navbar-events cf">
 	<div class="content">
-		<a class="back-button" href="home.php">regresar</a>
+		<a class="back-button" href="/">regresar</a>
 		<img class="navbar-logo" src="img/actua.svg">
 	</div>
 </header>
