b3e5ca5b8bcfdd9303451dd5d8bcee6eca6f2bc1
Hector Rincon <hecerinc@hotmail.com>
2018-03-12 17:22:20 -0600

    Add analytics. Fix #10

--- footer.php
+++ footer.php
@@ -1,2 +1,12 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115610738-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-115610738-1');
+</script>
+
 </body>
 </html>
